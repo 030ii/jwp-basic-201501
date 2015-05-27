354926d9bdd4810a33b6390b554970b22641c055
030ii <lovelygeee2@gmail.com>
2015-05-27 22:10:26 +0900

    5번. 답변하기 기능, 답변 수 증가하는 DB로직 추가

--- src/main/java/core/mvc/RequestMapping.java
+++ src/main/java/core/mvc/RequestMapping.java
@@ -3,6 +3,7 @@ package core.mvc;
 import java.util.HashMap;
 import java.util.Map;
 
+import next.controller.AddAnswerController;
 import next.controller.AddQuestionController;
 import next.controller.ListController;
 import next.controller.ShowController;
@@ -18,6 +19,7 @@ public class RequestMapping {
 		mappings.put("/save.next", new AddQuestionController()); // 3번 - 질문하기 기능 
 		mappings.put("/list.next", new ListController());
 		mappings.put("/show.next", new ShowController());
+		mappings.put("/api/addanswer.next", new AddAnswerController()); // 5번 - 답변하기 기
 		mappings.put("/form.next", new ForwardController("form.jsp"));
 		
 		logger.info("Initialized Request Mapping!");
--- src/main/java/next/controller/AddAnswerController.java
+++ src/main/java/next/controller/AddAnswerController.java
@@ -3,26 +3,29 @@ package next.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import next.dao.AnswerDao;
 import next.dao.QuestionDao;
-import next.model.Question;
+import next.model.Answer;
 import core.mvc.AbstractController;
 import core.mvc.ModelAndView;
 import core.utils.ServletRequestUtils;
 
-public class AddQuestionController extends AbstractController {
+public class AddAnswerController extends AbstractController {
+	private AnswerDao answerDao = new AnswerDao();
 	private QuestionDao questionDao = new QuestionDao();
 
 	@Override
 	public ModelAndView execute(HttpServletRequest request, 
 			HttpServletResponse response) throws Exception {
 		String writer = ServletRequestUtils.getRequiredStringParameter(request, "writer");
-		String title = ServletRequestUtils.getRequiredStringParameter(request, "title");
 		String contents = ServletRequestUtils.getRequiredStringParameter(request, "contents");
+		Long questionId = ServletRequestUtils.getRequiredLongParameter(request, "questionId");
 
-		Question question = new Question(writer, title, contents);
-		questionDao.insert(question);
+		Answer answer = new Answer(writer, contents, questionId);
+		answerDao.insert(answer); // 답변 등
+		questionDao.increaseCountOfComment(questionId); // 답변수 증가 
 		
-		ModelAndView mav = jstlView("redirect:/list.next"); // 질문 목록 페이지를 다시 보여줘!
+		ModelAndView mav = jstlView("redirect:/"); // 새로고침!
 		return mav;
 	}
 }
