997dce328ac75d6f95ddc35a4ddac3c5c3d1821a
030ii <lovelygeee2@gmail.com>
2015-06-05 13:25:43 +0900

    7번. ListController과 ShowController 메소드 안에 객체 선언

--- src/main/java/next/controller/ListController.java
+++ src/main/java/next/controller/ListController.java
@@ -11,13 +11,11 @@ import core.mvc.AbstractController;
 import core.mvc.ModelAndView;
 
 public class ListController extends AbstractController {
-	private QuestionDao questionDao = new QuestionDao();
-	private List<Question> questions;
-	
 	@Override
 	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		questions = questionDao.findAll();
+		QuestionDao questionDao = new QuestionDao();
+		List<Question> questions = questionDao.findAll();
 		
 		ModelAndView mav = jstlView("list.jsp");
 		mav.addObject("questions", questions);
